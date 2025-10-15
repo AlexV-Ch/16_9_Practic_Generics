8e126d6af3e69ee79270df2d9c6b298e3efa6f84
AlexV-Ch <alexch@yandex.ru>
2025-10-15 22:32:42 +0300

    Начало 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // нажмите на значок <icon src="AllIcons.Actions.Execute"/> в поле.
 public class Main {
     public static void main(String[] args) {
-        String text = "Текст сообщения!";
+        String text = "Текст сообщения !";
         Notification<String> email = new EmailNotification();
         String result = send(text, email);
         System.out.println(result);
